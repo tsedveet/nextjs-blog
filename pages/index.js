18440ecb66387bcaf7238e9447b997d39faafec5
tsedveet <tsedvee.tt@gmail.com>
2021-12-17 17:37:06 +0800

    joohon zasvar

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Layout, { siteTitle } from "../components/layout";
 import utilStyles from "../styles/utils.module.css";
+import Image from "next/image";
 
 export default function Home() {
   return (
@@ -12,7 +13,7 @@ export default function Home() {
         <p>I'm Tsedvee. I'm a software engineer and gamer.</p>
         <p>
           За тэгээд зүгээр үзэж сурсан юмаараа нэг ийм юм хийж үзлээ. Онжого
-          харж байвал "LIKE" дархаа битий мартаарай
+          харж байвал "LIKE" дархаа битгий мартаарай.
         </p>
         <p>
           <a href="https://www.facebook.com/WoofWoofTV/posts/2073799783012518">
@@ -20,6 +21,14 @@ export default function Home() {
           </a>{" "}
           😊😊😊
         </p>
+        <p>Hello! Knock! knock! Хаалгаа! 😊</p>
+        <Image
+          priority
+          src="/images/3.jpg"
+          className={utilStyles.borderCircle}
+          height={300}
+          width={300}
+        />
         {/* <p>
           (This is a sample website - you’ll be building a site like this on{" "}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
